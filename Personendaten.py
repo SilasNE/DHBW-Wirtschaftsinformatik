6f28be2e2780f95104fb64f9622bbe206e4a3d6c
Silas Engel <silas.engel06@tutanota.com>
2025-10-08 17:27:55 +0200

    Delete Personendaten.py

--- Personendaten.py
+++
@@ -1,7 +0,0 @@
-x = str(input("Wie heißt du: "))
-
-y = int(input("Wie alt bist du: "))
-
-z = int(input("Wie viel verdienst du: "))
-
-print(f"Dein Name ist: {x} \n" + f"Du bist {y} " + "Jahre alt. \n" + f"Du verdienst {z} \n")
