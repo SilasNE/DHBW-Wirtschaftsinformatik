96d3f38bc454503328df4cd1234d7ff527f4973f
Silas Engel <silas.engel06@tutanota.com>
2025-10-31 09:07:05 +0100

    menue.py erstellen

---
+++ menue.py
@@ -0,0 +1,14 @@
+while True:
+    i = int(input("Bitte wählen Sie eine Option:\n1. 1Funktion 1\n2. 2Funktion 2\n3. 3Funktion 3\n4. Programmbeenden \n")) 
+    if i == 1:
+        i = int(input("Bitte wählen Sie eine Option:\n1. 1Funktion 1\n2. 2Funktion 2\n3. 3Funktion 3\n4. Programmbeenden \n"))
+        
+    elif i == 2:
+        i = int(input("Bitte wählen Sie eine Option:\n1. 1Funktion 1\n2. 2Funktion 2\n3. 3Funktion 3\n4. Programmbeenden \n"))
+        
+    elif i == 3:
+        i = int(input("Bitte wählen Sie eine Option:\n1. 1Funktion 1\n2. 2Funktion 2\n3. 3Funktion 3\n4. Programmbeenden \n"))
+        
+    elif i == 4:
+        print("Programm wird beendet.")
+        break
